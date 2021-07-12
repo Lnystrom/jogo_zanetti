e45e1099fd4b83b2e84f7196733d145c11c1450e
Lnystrom <49409322+Lnystrom@users.noreply.github.com>
2021-07-11 21:01:39 -0300

    Adicionado novas funções

--- jogo/main.c
+++ jogo/main.c
@@ -16,46 +16,61 @@ ALLEGRO_BITMAP *capa = NULL;
 void * __gxx_personality_v0=0;
 void * _Unwind_Resume =0;
 
+
+
+void allegro()
+{
+//iniciando allegro, para receber input:
+    if (!al_init())
+    {
+        printf("Não foi possível executar o allegro.\n");
+    }
+    if (!al_install_keyboard())
+    {
+        printf("Não foi possível executar o teclado.\n");
+    }
+    if (!al_install_mouse())
+    {
+        printf("Não foi possível executar o mouse.\n");
+    }
+      al_init_image_addon();
+}
+
+void finalizar_allegro()
+{
+    al_destroy_display(display);
+    al_uninstall_system();
+    al_destroy_bitmap(capa);
+}
+
+/*void desenhar()
+{
+
+}
+*/
+
 int main(int argc, char **argv)
 {
 //acentuação
-setlocale(LC_ALL,"");
+    setlocale(LC_ALL,"");
 
-//iniciando allegro, para receber input:
-if (!al_init())
-    {
-      printf("Não foi possível executar o allegro.\n");
-    }
-if (!al_install_keyboard())
-    {
-      printf("Não foi possível executar o teclado.\n");
-    }
-if (!al_install_mouse())
-    {
-      printf("Não foi possível executar o mouse.\n");
-    }
 
 //criando uma janela
-display = al_create_display(800, 450);
+    display = al_create_display(800, 450);
 
 //criando imagem de capa
-al_init_image_addon();
-capa = al_load_bitmap("imagens/landscape.bmp");
+allegro();
 
-if (!capa)
+    capa = al_load_bitmap("imagens/landscape.bmp");
+
+    if (!capa)
     {
-      printf("Erro em carregar imagens/landscape.bmp\n");
+        printf("Erro em carregar imagens/landscape.bmp\n");
     }
-al_draw_bitmap(capa,0,0,0);
-al_flip_display(); //"Copies or updates the front and back buffers so that what has been drawn previously"
-al_rest(10.0);
-al_destroy_display(display);
-al_destroy_bitmap(capa);
-al_uninstall_system();
+    al_draw_bitmap(capa,0,0,0);
+    al_flip_display(); //"Copies or updates the front and back buffers so that what has been drawn previously"
+    al_rest(10.0);
 
-
-return 0;
+    return 0;
 }
-
-
 
