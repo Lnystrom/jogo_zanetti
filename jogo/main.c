51cb2b477c13ec55c9d84faf7dc9977b64dab630
Leuryssom Nystrom <apleuryssomrs@gmail.com>
2021-07-23 03:09:21 -0300

    adicionado música ao menu, porém um bug fecha o buffer segundos após. Acho que há mal alocamento de memória

--- jogo/main.c
+++ jogo/main.c
@@ -1,47 +1,52 @@
 //roubado do zanetti
 
+#include <locale.h>
 #include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
-#include <locale.h>
+#include <allegro5/allegro_audio.h>
+#include "allegro5/allegro_acodec.h"
 
-
-//Vari·veis criadas quando houver necessidade
+//Vari√°veis criadas quando houver necessidade
 ALLEGRO_EVENT_QUEUE *fila = NULL;
 ALLEGRO_TIMER *timer = NULL;
 
-
-//vari·veis de imagens
+//vari√°veis de imagens
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_BITMAP *capa = NULL;
-
 
 //tela de carregamento
 ALLEGRO_BITMAP *carregamento = NULL;
 
 //usado para resolver um bug
-void * __gxx_personality_v0=0;
-void * _Unwind_Resume =0;
-
-
+void *__gxx_personality_v0 = 0;
+void *_Unwind_Resume = 0;
 
 void allegro()
 {
-//iniciando allegro, para receber input:
+    //iniciando allegro, para receber input:
     if (!al_init())
     {
-        printf("N„o foi possÌvel executar o allegro.\n");
+        printf("N√£o foi poss√≠vel executar o allegro.\n");
     }
     if (!al_install_keyboard())
     {
-        printf("N„o foi possÌvel executar o teclado.\n");
+        printf("N√£o foi poss√≠vel executar o teclado.\n");
     }
     if (!al_install_mouse())
     {
-        printf("N„o foi possÌvel executar o mouse.\n");
+        printf("N√£o foi poss√≠vel executar o mouse.\n");
     }
+    if (!al_install_audio())
+    {
+        printf("N√£o foi poss√≠vel instalar o √°udio.\n");
+    }
+
     //criando uma janela
+    al_set_new_display_flags(ALLEGRO_FRAMELESS);
     display = al_create_display(800, 450);
+    al_set_window_position(display, 200, 200);
+    al_set_window_title(display, "O b√™bado e o equilibrista");
     al_init_image_addon();
 }
 
@@ -53,30 +58,46 @@ void tela_de_carregamento()
     for (contador = 1; contador < quadros; contador++)
     {
         sprintf(frame, "imagens/frames/frame_%.2d_delay-0.1s.gif", contador);
-        // ("%s\n", frame); --> ver se est· funcionado printf
+        // ("%s\n", frame); --> ver se est√° funcionado printf
         carregamento = al_load_bitmap(frame);
         if (!carregamento)
         {
             printf("Erro em carregar %s\n", frame);
         }
-        al_draw_bitmap(carregamento,300,200,0);
+        al_draw_bitmap(carregamento, 300, 200, 0);
         al_flip_display(); //"Copies or updates the front and back buffers so that what has been drawn previously"
         al_rest(0.1);
+        al_destroy_bitmap(carregamento);
     }
-
 }
 
+void audio_menu()
+{
+    al_init_acodec_addon();
+    ALLEGRO_SAMPLE *tema_sample;
+    al_reserve_samples(1);
+    tema_sample = al_load_sample("audio/tema.wav");
+    al_play_sample(tema_sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP,0);
+}
 void fundo()
 {
-
-    capa = al_load_bitmap("imagens/landscape.bmp");
-
-    if (!capa)
+    ALLEGRO_KEYBOARD_STATE teclado;
+    while(1)
     {
-        printf("Erro em carregar imagens/landscape.bmp\n");
+        if (al_key_down(&teclado, ALLEGRO_KEY_ESCAPE))
+        {
+            break;
+        }
+        audio_menu();
+        capa = al_load_bitmap("imagens/landscape.bmp");
+        if (!capa)
+        {
+            printf("Erro em carregar imagens/landscape.bmp\n");
+        }
+        al_draw_bitmap(capa, 0, 0, 0);
+        al_flip_display(); //"Copies or updates the front and back buffers so that what has been drawn previously"
     }
-    al_draw_bitmap(capa,0,0,0);
-    al_flip_display(); //"Copies or updates the front and back buffers so that what has been drawn previously"
+
 }
 void finalizar_allegro()
 {
@@ -88,25 +109,21 @@ void finalizar_allegro()
 int main(int argc, char **argv)
 {
 
-//acentuaÁ„o
-    setlocale(LC_ALL,"");
+    //acentua√ß√£o
+    setlocale(LC_ALL, "Portuguese");
 
-
-//iniciando allegro
+    //iniciando allegro
     allegro();
-    al_rest(5.0);
 
-//criando tela de carregamento
+    //criando tela de carregamento
     tela_de_carregamento();
 
-//criando foto de capa
+    //criando foto de capa
     fundo();
 
     al_rest(10.0);
 
     finalizar_allegro();
 
-
     return 0;
 }
-
