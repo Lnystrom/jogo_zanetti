4a5cb573a57840110fc877dbdd648d4de900ee1f
Leuryssom Nystrom <apleuryssomrs@gmail.com>
2021-08-20 20:33:37 -0300

    adicionado parallax background

--- jogo/main.c
+++ jogo/main.c
@@ -12,6 +12,16 @@
 void *__gxx_personality_v0 = 0;
 void *_Unwind_Resume = 0;
 
+
+void funfa(bool teste, const char *descricao)
+{
+    if(teste)
+        return;
+
+    printf("couldn't initialize %s\n", descricao);
+    exit(1);
+}
+
 void tela_de_carregamento()
 {
     int contador;
@@ -31,16 +41,9 @@ void tela_de_carregamento()
         al_flip_display(); //"Copies or updates the front and back buffers so that what has been drawn previously"
         al_rest(0.1);
     }
+    al_destroy_bitmap(carregamento);
 }
 
-void funfa(bool teste, const char *descricao)
-{
-    if(teste)
-        return;
-
-    printf("couldn't initialize %s\n", descricao);
-    exit(1);
-}
 
 void menu(ALLEGRO_BITMAP *capa, ALLEGRO_FONT* font, ALLEGRO_SAMPLE* tema, ALLEGRO_KEYBOARD_STATE ks, int *game_state)
 {
@@ -50,28 +53,38 @@ void menu(ALLEGRO_BITMAP *capa, ALLEGRO_FONT* font, ALLEGRO_SAMPLE* tema, ALLEGR
     {
         al_draw_textf(font, al_map_rgb(255, 255, 255), 400, 480/2, ALLEGRO_ALIGN_CENTER, "Pressione Enter");
     }
-    al_play_sample(tema,1,1,1, ALLEGRO_PLAYMODE_LOOP, NULL);
+    al_play_sample(tema,1,0,1, ALLEGRO_PLAYMODE_LOOP, NULL);
     al_get_keyboard_state(&ks);
     if (al_key_down(&ks, ALLEGRO_KEY_ENTER))
     {
         *game_state = 1;
         al_destroy_sample(tema);
     }
-    /*else if (al_key_down(&ks, ALLEGRO_KEY_ESCAPE))
-    {
-        *game_state = 2;
-    }
-    */
+
 }
 
-void jogo(ALLEGRO_SAMPLE* tema, float x, float y, ALLEGRO_BITMAP *cenario)
+void cenario(ALLEGRO_BITMAP *parallax, float cenario_x)
+{
+        al_draw_bitmap(parallax, (int)cenario_x, 0, 0);
+
+}
+
+void jogo(ALLEGRO_SAMPLE* tema, float x, float y, ALLEGRO_BITMAP *cenario_3, ALLEGRO_BITMAP *cenario_2, ALLEGRO_BITMAP *cenario_1, const float FPS, int altura, int largura)
 {
     al_clear_to_color(al_map_rgb(0,0,0));
 
     //-----------------------------------------------------
+    static float cenario_x_1=0;
+    static float cenario_x_2=0;
+    static float cenario_x_3=0;
+    cenario_x_1 -=  50.0/FPS;
+    cenario_x_2 -=  75.0/FPS;
+    cenario_x_3 -=  100.0/FPS;
 
+    cenario( cenario_1, cenario_x_1);
+    cenario( cenario_2, cenario_x_2);
+    cenario( cenario_3, cenario_x_3);
 
-    al_draw_bitmap(cenario, 0, 0, 0);
     al_draw_filled_rectangle(x, y, x + 30, y + 30, al_map_rgb(255,255,0));
 
 }
@@ -81,7 +94,8 @@ int main(int argc, char **argv)
     funfa(al_init(), "allegro");
     funfa(al_install_keyboard(), "teclado");
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    const float FPS = 30.0;
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);
     funfa(timer, "timer");
 
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
@@ -91,9 +105,10 @@ int main(int argc, char **argv)
     int altura = 600;
 
     al_set_new_display_flags(ALLEGRO_FRAMELESS);
+   al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
     ALLEGRO_DISPLAY* disp = al_create_display(largura, altura);
     funfa(disp, "display");
-    al_set_window_position(disp, 100, 100);
+    //al_set_window_position(disp, 100, 100);
     al_set_window_title(disp, "O bêbado e o equilibrista");
 
     al_init_primitives_addon();
@@ -128,9 +143,22 @@ int main(int argc, char **argv)
     ALLEGRO_SAMPLE* tema = al_load_sample("audio/tema.wav");
     funfa(tema, "musica tema");
 
-    ALLEGRO_BITMAP *cenario;
-    cenario = al_load_bitmap("imagens/sprites/cenario_1.png");
-    funfa(cenario, "cenario");
+
+
+     ALLEGRO_BITMAP *cenario_1;
+    cenario_1 = al_load_bitmap("imagens/sprites/cenario_1.png");
+    funfa(cenario_1, "cenario 1");
+    al_convert_mask_to_alpha(cenario_1, al_map_rgb(255,255,255));
+
+     ALLEGRO_BITMAP *cenario_2;
+    cenario_2 = al_load_bitmap("imagens/sprites/cenario_2.png");
+    funfa(cenario_2, "cenario 2");
+    al_convert_mask_to_alpha(cenario_2, al_map_rgb(255,255,255));
+
+    ALLEGRO_BITMAP *cenario_3;
+    cenario_3 = al_load_bitmap("imagens/sprites/cenario_3.png");
+    funfa(cenario_3, "cenario 3");
+    al_convert_mask_to_alpha(cenario_3, al_map_rgb(255,255,255));
 
     ALLEGRO_KEYBOARD_STATE ks;
 
@@ -192,7 +220,7 @@ int main(int argc, char **argv)
         }
         else if(redraw && game_state == 1 && al_is_event_queue_empty(queue))
         {
-            jogo(tema, mov_x, mov_y, cenario);
+            jogo(tema, mov_x, mov_y, cenario_3, cenario_2, cenario_1, FPS, altura, largura);
             redraw = false;
         }
        /* else if (redraw && game_state == 2 && al_is_event_queue_empty(queue))
